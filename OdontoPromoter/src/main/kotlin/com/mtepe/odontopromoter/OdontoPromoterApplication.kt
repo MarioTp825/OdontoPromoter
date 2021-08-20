2345033b5476421872848498ad6e1c420d90f6b3
Mario Tepe <marioandresth@yahoo.com>
2021-08-19 23:42:57 -0600

    Create: creation of project

---
+++ OdontoPromoter/src/main/kotlin/com/mtepe/odontopromoter/OdontoPromoterApplication.kt
@@ -0,0 +1,11 @@
+package com.mtepe.odontopromoter
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OdontoPromoterApplication
+
+fun main(args: Array<String>) {
+    runApplication<OdontoPromoterApplication>(*args)
+}
---
+++ OdontoPromoter/src/test/kotlin/com/mtepe/odontopromoter/OdontoPromoterApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.mtepe.odontopromoter
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OdontoPromoterApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
